ec05aed0e18a05be98138b5103f0d85136e80047
zhiyanliu <liuzhiyan138@163.com>
2021-04-29 10:31:34 +0800

    find/style/each

---
+++ src/dom.js
@@ -0,0 +1,22 @@
+window.dom = {
+  find(selector, scope) {
+    return (scope || document).querySelectorAll(selector);
+  },
+  style(node, name, value) {
+    if (arguments.length === 3) {
+      node.style[name] = value;
+    } else if (arguments.length === 2) {
+      return node.style[name];
+    } else if (name instanceof Object) {
+      for (let key in name) {
+        node.style[key] = name[key];
+      }
+    }
+  },
+
+  each(nodeList, fn) {
+    for (let i = 0; i < nodeList.length; i++) {
+      fn.call(null, nodeList[i]);
+    }
+  },
+};
---
+++ src/main.js
@@ -0,0 +1,7 @@
+const div = dom.find("#test>.red")[0];
+console.log(div);
+
+dom.style(div, "color", "red");
+
+const divList = dom.find(".red");
+dom.each(divList, (n) => console.log(n));
